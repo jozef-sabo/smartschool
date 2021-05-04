ea0670bcb5ed74eb12d538bbca0f0cfd65f49d2f
Jozef Sabo <8414716-jozef-sabo@users.noreply.gitlab.com>
2021-05-04 19:45:27 +0200

    Using variable for SQL query instead of direct access

--- server/fetchData.py
+++ server/fetchData.py
@@ -10,14 +10,17 @@ config = {
     'raise_on_warnings': True
 }
 
+
 # param date, idClass
 def fetch(date, idClass, sensor):
     cnx = mysql.connector.connect(**config)
     cursor = cnx.cursor()
 
     # sql_query = "SELECT * FROM rooms WHERE (`date_time` > DATE_SUB(now(), INTERVAL 30 DAY))"
-    
-    cursor.execute("SELECT date_time, sensor_value FROM rooms WHERE CAST(`date_time` AS DATE)  = (%s) AND `room_number` = (%s) AND `sensor_type` = (%s)", (date, idClass, sensor))
+    sql_query = "SELECT `date_time`, `sensor_value` FROM rooms WHERE CAST(`date_time` AS DATE)  = \'%s\' AND " \
+                "`room_number` = \'%s\' AND `sensor_type` = \'%s\'" % (date, idClass, sensor)
+
+    cursor.execute(sql_query)
 
     data = cursor.fetchall()
 
